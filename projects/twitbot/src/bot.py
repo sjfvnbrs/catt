16470b756787d702d748fd8872c4b58ac1ffff78
sjfvnbrs <sjefvanbeers@Sjef-van-Beerss-MacBook-Pro.local>
2015-06-10 12:35:37 +0200

    worked on this with Luis
    worked on this with Luis

--- projects/twitbot/src/bot.py
+++ projects/twitbot/src/bot.py
@@ -4,6 +4,7 @@ import tweepy
 from keys import keys
 from pprint import pprint
 import time
+import random
 
 CONSUMER_KEY = keys['consumer_key']
 CONSUMER_SECRET = keys['consumer_secret']
@@ -11,6 +12,9 @@ ACCESS_TOKEN = keys['access_token']
 ACCESS_TOKEN_SECRET = keys['access_token_secret']
 
 api = None
+
+
+strollers = ["@bugaboo", "@maxicosiusa", "@stokkebaby", "@easywalkernl","@urbinibaby", "@kolcraft"]
 
 def connect():
 	global api
@@ -22,7 +26,7 @@ def connect():
 def search():
 	global api
 	twts = api.search(q="Hello World!")
-	pprint(twts[0])
+	#pprint(twts[0])
 	print "*"*80
 	print twts[0].text
 	# for t in twts:
@@ -42,14 +46,31 @@ def tweetforever():
 	filename.close()
 
 	for line in f:
-	     api.update_status(line)
+	     api.update_status(status=line)
 	     print line
-	     time.sleep(3600) # Sleep for 1 hour
+	     time.sleep(60) # Sleep for 1 hour
+
+def respond_to_pussy():
+	while True:
+		try:
+			twts = api.search(q="pussy like a stroller")
+			if twts:
+				for t in twts:
+					screename = t.user.screen_name.encode('utf-8')
+					anystroller = random.choice(strollers)
+					mytweet = "@{0} maybe {1} could help you with that".format(screename, anystroller)
+					print "about to tweet: ", mytweet
+					api.update_status(status=mytweet)
+		except tweepy.error.TweepError, e:
+			pass
+
 
 def main():
 	connect()
-	search()
-	mentions()
+	respond_to_pussy()
+	#tweetforever()
+	#search()
+	#mentions()
 
 if __name__ == "__main__":
 	main()
